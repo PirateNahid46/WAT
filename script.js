124749ff920937549fb8563d88949a725088a657
PirateNahid <piratenahid46@gmail.com>
2022-07-29 14:34:27 +0600

    v

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 var x = -1;
-var Words = ["Hi", "Hello"];
+var Words = ["Displaced", "Sex", "Virgin", "Girl", "Grief", "Dash", "Women", "Dedicate", "Misfortune", "Luck", "Sound", "Light", "Sun", "Honest"];
 
-
+let audio = new Audio();
 
 let myVar = setInterval(myTimer ,10000);
 function myTimer() {
   x++;
+  audio.src = "src/sound.mp3";
   document.getElementById("wat").innerHTML = Words[Math.floor(Math.random() * Words.length)];
+  audio.play();
   
-  if(x == 80){
+  if(x == 81){
   	clearInterval(myVar);
+    document.getElementById("wat").innerHTML = "Finished";
 	}
 }
