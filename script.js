007918b11ba41d36f27895192ff103e4f88531c0
piratenahid46@gmail.com <piratenahid46@gmail.com>
2022-08-15 07:00:42 +0600

    haha

--- script.js
+++ script.js
@@ -2,6 +2,20 @@ var x = -1;
  let Words;
 const set1 = ["Displaced", "Sex", "Virgin", "Girl", "Grief", "Dash", "Women", "Dedicate", "Misfortune", "Luck", "Sound", "Light", "Sun", "Honest", "Word", "Hard", "People", "Success", "Mind", "Late", "Secret", "Stone", "Money", "Clash", "Rose", "Morning", "Sleep", "Game", "Ear", "Travel", "Figure", "Spider", "Sincerity", "Seniors", "Efficient", "Bullet", "Lonely", "Equal", "Request", "Assemble", "Field", "Week", "Miss", "Dawn", "Success", "Gentle", "Need", "Cry", "Push", "Task", "Quarelling", "Challenge", "Keen", "Rumour", "Crazy", "Zero", "Joy", "Pale", "Society", "State", "Designation", "President", "Blow", "Check", "Alarm", "Corruption", "Newspaper", "Sorrow", "Death", "Judge", "Noble", "Guitar", "Pious", "Favour", "Love", "Pull", "Fool", "Lesson", "Fight", "Shoe", "Waste", "Dear", "Brains", "Action", "Annoy", "Honesty", "Save", "Native", "Refuse", "Low", "Taught", "Grass", "Crowd", "Lip", "Police", "Study", "Wide", "Surplus", "Discipline", "Clerk", "Respect", "Father", "Embrace", "Gun", "Freedom", "Glad", "Ship", "Hate", "Bring", "Coup", "Cycle", "Air", "Loose", "Sword", "Power", "Lonely", "Hunting", "Tea"];
 
+
+function splitToChunks(array, parts) {
+  let result = [];
+  const chunkSize = parts;
+  for (let i = 0; i < array.length; i += chunkSize) {
+    const chunk = array.slice(i, i + chunkSize);
+    result.push(chunk);
+  }
+  console.log(result);
+  return result;
+}
+
+
+
 let audio = new Audio();
 audio.src = "src/sound.mp3";
 let myVar;
@@ -11,14 +25,9 @@ function start(){
  
   var time = document.getElementById("time").value;
   var setV = document.getElementById("set").value;
-  switch(setV){
-    case "1":
-      Words = set1;
-      break;
-    case "2":
-      alert("Coming Soon");
-      break;
-  }
+  
+  var ArrayM = splitToChunks(set1, 80);
+  Words = ArrayM[setV-1];
   
   document.getElementById("wat").innerHTML = "Wait for "+time+"s";
   myVar = setInterval(myTimer ,time*1000);
